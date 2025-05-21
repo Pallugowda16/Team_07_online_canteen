0cffd200fc922fa19af782c671570f1f3a19c4c6
Pallugowda16 <pallavi.s2@s.amity.edu>
2025-05-21 11:29:07 +0530

    const email

--- script.js
+++ script.js
@@ -14,4 +14,4 @@ function togglePassword() {
 document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-
+  const email = document.getElementById("email").value.trim();
